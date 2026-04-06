209e07bf9551941a61e27b3cd225b40523640b2a
n1k1tal0x <d.cuzya07@gmail.com>
2026-04-06 10:25:57 +0300

    fc

--- hub_log_2.4/Program.cs
+++ hub_log_2.4/Program.cs
@@ -6,6 +6,19 @@ namespace hub_log_2._4
     {
         private static ILogger Logger = new LoggerConfiguration().WriteTo.Console().WriteTo.File($"logs/log-{DateTimeOffset.UtcNow.ToUnixTimeSeconds()}").CreateLogger();
         private static IEnumerable<string> taskList;
+        private static bool _appCicle = true;
+        private static void AddTaskEvent()
+        {
+
+        }
+        private static void RemoveTaskEvent()
+        {
+
+        }
+        private static void GetTasksEvent()
+        {
+
+        }
         static void Main(string[] args)
         {
             try
@@ -14,21 +27,26 @@ namespace hub_log_2._4
                 Logger.Information("App start");
                 taskList = new List<string>();
                 Logger.Information("Init seccuess");
-                while (true)
+                while (_appCicle)
                 {
                     Console.WriteLine(@"Enter action:
     1 - Add task
     2 - Remove task
-    3 - Show task list");
+    3 - Show task list
+    4 - Close app");
                     var input = Console.ReadLine();
                     Logger.Information($"User input: {input}");
                     switch (input.ToLower().Trim())
                     {
                         case "1":
+                            AddTaskEvent();
                             break;
                         case "2":
                             break;
                         case "3":
+                            break;
+                        case "4":
+                            _appCicle = true;
                             break;
                     }
                 }
