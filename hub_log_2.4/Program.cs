26af375c59c4b704dc650c63af87eda2e374ae4f
n1k1tal0x <d.cuzya07@gmail.com>
2026-04-13 12:47:22 +0300

    update

--- hub_log_2.4/Program.cs
+++ hub_log_2.4/Program.cs
@@ -1,4 +1,5 @@
 ﻿using Serilog;
+using System.Diagnostics;
 
 namespace hub_log_2._4
 {
@@ -9,6 +10,8 @@ namespace hub_log_2._4
         private static bool _appCicle = true;
         private static void AddTaskEvent()
         {
+            Logger.Debug($"[AddTaskEvent] Start event");
+            var stopwatch = Stopwatch.StartNew();
             try
             {
                 while (true)
@@ -16,23 +19,30 @@ namespace hub_log_2._4
                     Console.WriteLine("Enter task text:");
                     string input = Console.ReadLine();
                     Logger.Information($"[AddTaskEvent] User input: {input}");
+                    
                     if (!String.IsNullOrEmpty(input))
                     {
                         taskList.Add(input);
                         Logger.Information($"[AddTaskEvent] Task was added");
                         return;
                     }
+
                     Console.WriteLine("Bad input");
-                    Logger.Information($"[AddTaskEvent] Bad input");
+                    Logger.Information($"[AddTaskEvent] Bad input (input: {input})");
                 }
             } catch (Exception ex)
             {
+                Logger.Debug($"[AddTaskEvent] Error");
                 Logger.Error(ex.Message);
                 Logger.Error(ex.StackTrace);
             }
+            stopwatch.Stop();
+            Logger.Debug($"[AddTaskEvent] End event (time: {stopwatch.ElapsedMilliseconds}ms)");
         }
         private static void RemoveTaskEvent()
         {
+            Logger.Debug($"[RemoveTaskEvent] Start event");
+            var stopwatch = Stopwatch.StartNew();
             try
             {
                 while (true)
@@ -50,27 +60,37 @@ namespace hub_log_2._4
                             Logger.Information($"[RemoveTaskEvent] Task was removed");
                             return;
                         }
+                        Logger.Error($"[RemoveTaskEvent] Index {index} don't exist");
+                    } else
+                    {
+                        Logger.Error($"[RemoveTaskEvent] Input isn't Int");
                     }
                     Console.WriteLine("Bad input");
-                    Logger.Information($"[RemoveTaskEvent] Bad input");
+                    Logger.Information($"[RemoveTaskEvent] Bad input (input: {input})");
                 }
             } catch (Exception ex)
             {
+                Logger.Debug($"[RemoveTaskEvent] Error");
                 Logger.Error(ex.Message);
                 Logger.Error(ex.StackTrace);
             }
+            stopwatch.Stop();
+            Logger.Debug($"[RemoveTaskEvent] End event (time: {stopwatch.ElapsedMilliseconds}ms)");
         }
         private static void GetTasksEvent()
         {
             Console.WriteLine("Task list (index, text):");
-            Logger.Information("Task list:");
+            Logger.Information("[GetTasksEvent] Start event");
+            var stopwatch = Stopwatch.StartNew();
             int index = 0;
             foreach (var task in taskList)
             {
                 Console.WriteLine($"\t{index} - {task}");
-                Logger.Information($"\t{index} - {task}");
+                Logger.Information($"[GetTasksEvent] [{index}] {task}");
                 index++;
             }
+            stopwatch.Stop();
+            Logger.Information($"[GetTasksEvent] End event (time: {stopwatch.ElapsedMilliseconds}ms)");
         }
         static void Main(string[] args)
         {
@@ -113,8 +133,10 @@ namespace hub_log_2._4
                 }
             } catch (Exception ex)
             {
+                Logger.Error("Error in Main()");
                 Logger.Fatal(ex.Message);
                 Logger.Fatal(ex.StackTrace);
+                Logger.Error("End error");
             }
             Logger.Information("App closed");
             Log.CloseAndFlush();
